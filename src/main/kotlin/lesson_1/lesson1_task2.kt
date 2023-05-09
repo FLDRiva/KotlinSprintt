fac4531966bbb29fcebf01dd3b7f312b624190f1
RivaSR <westly.revin@yandex.ru>
2023-05-09 17:27:54 +0400

    KS - 1 - 2 1. I output the values of the first task to the console 2. Created a new variable and assigned it a value 3. Output commented out a new variable 4. Assigned a different value to the variable numberOfWorkers and output it to the console

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,16 @@
+package lesson_1
+
+fun main() {
+    val numberOfOrders = 10
+    val textOrder = "Спасибо за покупку!"
+    println(numberOfOrders)
+    println(textOrder)
+
+    var numberOfWorkers = 2000
+//    println(numberOfWorkers)
+
+    numberOfWorkers = 1999
+    println(numberOfWorkers)
+
+
+}
