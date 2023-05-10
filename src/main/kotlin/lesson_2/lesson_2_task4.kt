5a70336d8f084c99ef8a5ce6ba23a446f9d77d15
RivaSR <westly.revin@yandex.ru>
2023-05-10 14:23:44 +0400

    KS - 2 - 4 Calculated the number of resources that are given with a buff

---
+++ src/main/kotlin/lesson_2/lesson_2_task4.kt
@@ -0,0 +1,14 @@
+package lesson_2
+
+fun main() {
+
+    val crystal = 7
+    val steel = 11
+    val buff = 20
+
+    val crystalBuff = crystal * buff / 100
+    val steelBuff = steel * buff / 100
+
+    println(crystalBuff)
+    println(steelBuff)
+}
