44d22ac5174abcca9263e8aa22e96585bf2c7e39
RivaSR <westly.revin@yandex.ru>
2023-05-10 18:46:42 +0400

    KS - 1 - 4 Condition fulfilled

---
+++ src/main/kotlin/lesson1/lesson1_task4.kt
@@ -0,0 +1,19 @@
+package lesson1
+
+fun main() {
+
+    val lenght: Long = 40_868_600_000
+    val age: Byte = 27
+    val partOfDay: Double = 0.075
+    val seconds: Short = 6480
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogree: Int = 327000
+
+    println(lenght)
+    println(age)
+    println(partOfDay)
+    println(seconds)
+    println(partOfYear)
+    println(apogree)
+
+}
