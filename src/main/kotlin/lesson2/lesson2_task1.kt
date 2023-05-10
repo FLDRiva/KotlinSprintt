7bb551ded392f7541772058a8ea3f5432d01be82
RivaSR <westly.revin@yandex.ru>
2023-05-10 19:33:56 +0400

    KS - 2 - 1 Condition fulfilled

---
+++ src/main/kotlin/lesson2/lesson2_task1.kt
@@ -0,0 +1,13 @@
+package lesson2
+
+fun main() {
+
+    val human1 = 3
+    val human2 = 4
+    val human3 = 3
+    val human4 = 5
+    val average = human1.toDouble() + human2.toDouble() + human3.toDouble() + human4.toDouble() / 4
+
+    println(average)
+
+}
