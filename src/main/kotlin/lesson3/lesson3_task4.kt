8b9088dc748f9b3c4255214fd20eef9cec096b6b
RivaSR <westly.revin@yandex.ru>
2023-05-11 12:35:30 +0400

    KS - 3 - 4 The task is completed

---
+++ src/main/kotlin/lesson3/lesson3_task4.kt
@@ -0,0 +1,15 @@
+package lesson3
+
+fun main() {
+
+    var move1 = "E2"
+    var move2 = "E4"
+    var number = "Ход №1"
+    println("$number - из $move1 на $move2")
+
+    move1 = "D2"
+    move2 = "E2"
+    number = "Ход №2"
+    println("$number - из $move1 на $move2")
+
+}
