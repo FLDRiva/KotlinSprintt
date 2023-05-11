f7171b74aeb3c2f7c20b7d14b1971392f2900c0f
RivaSR <westly.revin@yandex.ru>
2023-05-11 11:34:29 +0400

    KS - 1 - 5 Solved the problem using String.format

--- src/main/kotlin/lesson1/lesson1_task5.kt
+++ src/main/kotlin/lesson1/lesson1_task5.kt
@@ -1,10 +1,16 @@
 package lesson1
 
+var seconds = 6480
 fun main() {
 
-    val seconds = 108
-    val minute = seconds / 60
-    val seconds1 = seconds.toString().dropLast(1)
-    println("0$minute:$seconds1")
+    println(timeOfGagarin())
+
+}
+
+fun timeOfGagarin (): String {
+
+    val minute = (seconds % 3600) / 60
+    val second = seconds % 60
+    return String.format("%02d:%02d", minute, second)
 
 }
