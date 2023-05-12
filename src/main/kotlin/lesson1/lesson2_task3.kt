123937213bb150559f58ab618ea0319849fb4113
RivaSR <westly.revin@yandex.ru>
2023-05-12 10:57:31 +0400

    KS - 2 - 3 editorial comments

--- src/main/kotlin/lesson1/lesson2_task3.kt
+++ src/main/kotlin/lesson1/lesson2_task3.kt
@@ -1,15 +1,12 @@
 package lesson1
-
 fun main() {
 
     val onTheWay = 457
     val exitMinute = 9
     val exitSeconds = 39
     val travelTimeMinute = onTheWay + exitMinute + (exitSeconds / 60)
-    val TimeHour = travelTimeMinute / 60
-    val TimeMinute = travelTimeMinute - (TimeHour * 60)
+    val timeHour = travelTimeMinute / 60
+    val timeMinute = travelTimeMinute - (timeHour * 60)
 
-
-    println("Поезд проехал за $TimeHour:$TimeMinute")
-
+    println("Поезд проехал за $timeHour:$timeMinute")
 }
