99faddca5d604d1faf9b3bed4e825aac97a5bb2b
RivaSR <westly.revin@yandex.ru>
2023-05-12 10:42:22 +0400

    KS - 1 - 5 took out a constant

--- src/main/kotlin/lesson1/lesson1_task5.kt
+++ src/main/kotlin/lesson1/lesson1_task5.kt
@@ -1,16 +1,13 @@
 package lesson1
 
-var seconds = 6480
+const val seconds = 6480
 fun main() {
 
     println(timeOfGagarin())
-
 }
-
 fun timeOfGagarin (): String {
 
     val minute = (seconds % 3600) / 60
     val second = seconds % 60
     return String.format("%02d:%02d", minute, second)
-
 }
