88f1c165f5d80a51da6ad40bf676ca4d8a118283
RivaSR <westly.revin@yandex.ru>
2023-05-12 11:17:53 +0400

    KS - 3 - 4 correction of comments

--- src/main/kotlin/lesson3/lesson3_task4.kt
+++ src/main/kotlin/lesson3/lesson3_task4.kt
@@ -2,14 +2,14 @@ package lesson3
 
 fun main() {
 
-    var move1 = "E2"
-    var move2 = "E4"
-    var number = "Ход №1"
-    println("$number - из $move1 на $move2")
+    var moveOut = "E2"
+    var moveIn = "E4"
+    var number = "номер хода №1"
+    println("$number - откуда $moveOut куда $moveIn")
 
-    move1 = "D2"
-    move2 = "E2"
-    number = "Ход №2"
-    println("$number - из $move1 на $move2")
+    moveOut = "D2"
+    moveIn = "E2"
+    number = "номер хода №2"
+    println("$number - откуда $moveOut куда $moveIn")
 
 }
