78f1bc7c7727e813e3c1fc82ed63d9e5ee8f0389
RivaSR <westly.revin@yandex.ru>
2023-05-15 12:02:20 +0400

    KS - 4 - 2 Fixed a bug

--- src/main/kotlin/lesson4/lesson4_task2.kt
+++ src/main/kotlin/lesson4/lesson4_task2.kt
@@ -2,17 +2,17 @@ package lesson4
 
 fun main() {
 
-    val weightMin = 30
-    val weightMax = 100
-    val value = 99
-
     val cargoOneWeight = 20
     val cargoTwoWeight = 50
     val cargoOneValue = 80
     val cargoTwoValue = 100
 
-    println("Average для груза с весом 20 кг и объемом 80 л: ${(weightMin < cargoOneWeight) && 
-            (cargoOneWeight <= weightMax) && (value > cargoOneValue)} \n" +
-            "Average для груза с весом 50 кг и объемом 100 л: ${(weightMin < cargoTwoWeight) && 
-                    (cargoTwoWeight <= weightMax) && (value > cargoTwoValue)}")
+    println("Average для груза с весом 20 кг и объемом 80 л: ${(WEIGHT_MIN < cargoOneWeight) && 
+            (cargoOneWeight <= WEIGHT_MAX) && (VALUE > cargoOneValue)} \n" +
+            "Average для груза с весом 50 кг и объемом 100 л: ${(WEIGHT_MIN < cargoTwoWeight) && 
+                    (cargoTwoWeight <= WEIGHT_MAX) && (VALUE > cargoTwoValue)}")
 }
+
+const val VALUE = 100
+const val WEIGHT_MAX = 100
+const val WEIGHT_MIN = 30
