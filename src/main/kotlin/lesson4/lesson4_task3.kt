ddfaaaea3dccaed0dbadd482fa240feadcd28367
RivaSR <westly.revin@yandex.ru>
2023-05-15 11:58:20 +0400

    KS - 4 - 3 Fixed a bug

--- src/main/kotlin/lesson4/lesson4_task3.kt
+++ src/main/kotlin/lesson4/lesson4_task3.kt
@@ -3,11 +3,11 @@ package lesson4
 fun main() {
 
     val weather = "солнечная погода"
-    val awningOptions = "тент раскрыт"
+    val isAwningOpen = true
     val humidity = 20
     val season = "зима"
 
-    val beanGrowth = (weather == "солнечная погода") && (awningOptions == "тент раскрыт") && (humidity == 20)
+    val beanGrowth = (weather == "солнечная погода") && (isAwningOpen) && (humidity == 20)
             && (season != "зима")
     println("Благоприятные ли условия сейчас для роста бобовых? $beanGrowth")
 }
