582a2e3a44e8fd1c79a6969f98c548a462939edf
RivaSR <westly.revin@yandex.ru>
2023-05-15 13:01:02 +0400

    KS - 5 - 1 task completed

---
+++ src/main/kotlin/lesson5/lesson5_task1.kt
@@ -0,0 +1,13 @@
+package lesson5
+
+fun main() {
+
+    println("Сколько будет 5 + 2?")
+    val example = readln().toInt()
+
+    if (example == 7) {
+        println("Добро пожаловать!")
+    } else {
+        println("Доступ запрещен")
+    }
+}
