9d1b943ec212998a5966d1d1133f91a221ada3c4
RivaSR <westly.revin@yandex.ru>
2023-05-15 12:37:53 +0400

    KS - 2 - 1 Made according to the conditions

--- src/main/kotlin/lesson2/lesson2_task1.kt
+++ src/main/kotlin/lesson2/lesson2_task1.kt
@@ -6,8 +6,7 @@ fun main() {
     val human2 = 4
     val human3 = 3
     val human4 = 5
-    val average = human1 + human2 + human3 + human4 / 4
+    val average = (human1 + human2 + human3 + human4) / 4.0
 
-    println(average.toDouble())
-
+    println(String.format("%.2f", average))
 }
