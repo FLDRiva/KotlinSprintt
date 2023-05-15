0566644bea481ab2b385cabc56f3c6849b2da490
RivaSR <westly.revin@yandex.ru>
2023-05-15 12:15:26 +0400

    KS - 3 - 2 Rewrote the code, removed the constants and the function

--- src/main/kotlin/lesson3/lesson3_task2.kt
+++ src/main/kotlin/lesson3/lesson3_task2.kt
@@ -1,18 +1,15 @@
 package lesson3
 
-const val name = "Татьяна"
-var surname = "Андреева"
-const val patronymic = "Сергеевна"
-var age = 20
-
 fun main() {
-    println("$surname $name $patronymic, $age лет")
-    println(getMarried())
-}
 
-fun getMarried() {
+    val name = "Татьяна"
+    var surname = "Андреева"
+    val patronymic = "Сергеевна"
+    var age = 20
+    println("$surname $name $patronymic, $age лет")
+
     surname = "Сидорова"
     age = 22
     println("$surname $name $patronymic, $age года")
-
 }
+
